920bf5a6d5b595883d683bbb7725259ff2a1d134
Japa Swadia <japa@podium.com>
2018-04-10 21:13:10 -0600

    db configuration

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,8 @@
 use Mix.Config
 config :fishing_spot, FishingSpot.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    database: "fishing_spot",
-    username: "fishing_spot",
-    password: "fishing_spot"
+  adapter: Ecto.Adapters.Postgres,
+  database: "fishing_spot",
+  username: "postgres",
+  password: "",
+  hostname: "localhost",
+  pool_size: 10
